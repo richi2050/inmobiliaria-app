a89c8638287b6b6cecba7e7903710c55618691ca
richi2050 <ricardo.lugo.recillas@gmail.com>
2020-03-18 22:32:02 -0600

    se agregaronb la Route segunda parte

--- src/componentes/layout/bar/BarSession.js
+++ src/componentes/layout/bar/BarSession.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Toolbar, Typography, withStyles, Button} from '@material-ui/core'
+import {Toolbar, Typography, withStyles, Button, IconButton} from '@material-ui/core'
 
 const styles = theme =>({
   sectionDesktop:{
@@ -7,6 +7,15 @@ const styles = theme =>({
     [theme.breakpoints.up("md")] :{
       display:"flex"
     }
+  },
+  sectionMobile: {
+    display:"flex",
+    [theme.breakpoints.up("md")] :{
+      display:"none"
+    }
+  },
+  grow: {
+    flexGrow:1
   }
 });
 
@@ -16,12 +25,22 @@ const styles = theme =>({
         return (
             <div>
                 <Toolbar>
+                  <IconButton color='inherit'>
+                    <i className='material-icons'>menu</i>
+                  </IconButton>
                     <Typography variant='h6'>
                         VXI HOME
                     </Typography>
+                    <div className={classes.grow}></div>
                     <div className={classes.sectionDesktop}>
                       <Button>Login</Button>
                     </div>
+                    <div className={classes.sectionMobile}>
+                        <IconButton color='inherit'>
+                          <i className='material-icons'>more_vert</i>
+                        </IconButton>
+                    </div>
+
                 </Toolbar>
             </div>
         )
